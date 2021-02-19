dcfe0a9212267d983806700dd02d126e9677373b
seoyoung78 <tjdud0708@naver.com>
2021-02-19 14:23:53 +0900

    2/19 오전 수업

---
+++ src/ch06/exam02/Car.java
@@ -0,0 +1,11 @@
+package ch06.exam02;
+
+public class Car {
+	//Field - 고유데이터
+	String company;
+	String model;
+	int price;
+	boolean old;
+	// 상태
+	int speed;
+}
---
+++ src/ch06/exam04/Korean.java
@@ -0,0 +1,16 @@
+package ch06.exam04;
+
+public class Korean {
+	//Field
+	String nation = "ÇÑ±¹";
+	String name;
+	String ssn;
+	
+	//Constructor
+	Korean(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+		
+	}
+	
+}
