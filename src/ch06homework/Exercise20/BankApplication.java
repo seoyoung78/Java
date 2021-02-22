c40940da9ecdf84989b0ed5272ab9f467d437d96
Yoon <Yoon@222.109.188.218>
2021-02-23 00:05:45 +0900

    과제 수정

--- src/ch06homework/Exercise20/BankApplication.java
+++ src/ch06homework/Exercise20/BankApplication.java
@@ -38,13 +38,13 @@ public class BankApplication {
 		System.out.println("---------");
 		
 		for (int i = 0; i <= accountArray.length; i++) {
-			if (accountArray[i] == null ) {
+			if (accountArray[i] == null) {
 				System.out.print("계좌변호: ");
 				accountArray[i].setAno(scanner.nextLine());
 				System.out.print("계좌주: ");
 				accountArray[i].setOwner(scanner.nextLine());
 				System.out.print("초기입금액: ");
-				accountArray[i].setBalance(scanner.nextLine());
+				accountArray[i].setBalance(scanner.nextInt());
 				System.out.println("결과: 계좌가 생성되었습니다.");
 				break;
 			}
@@ -59,9 +59,13 @@ public class BankApplication {
 		System.out.println("---------");
 		
 		for (int i = 0; i <= accountArray.length; i++) {
-			System.out.println(accountArray[i].getAno() + "\t"
-								+ accountArray[i].getOwner() + "\t"
-								+ accountArray[i].getBalance());
+			if (accountArray[i] != null) {
+				System.out.println(accountArray[i].getAno() + "\t"
+									+ accountArray[i].getOwner() + "\t"
+									+ accountArray[i].getBalance());
+				} else {
+					continue;
+			}
 		}
 	}
 	
@@ -69,20 +73,52 @@ public class BankApplication {
 	private static void deposit() {
 		System.out.println("----------");
 		System.out.println("예금");
+		
 		System.out.println("---------");
 		System.out.println("계좌번호: ");
 		
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray == null) {
+				continue;
+			} else if (accountArray[i].getAno().equals(scanner.nextLine())) {
+				System.out.println("예금액: ");
+				accountArray[i].setBalance(accountArray[i].getBalance() + scanner.nextInt());
+				break;
+			}
+		}
 		
 		System.out.println("결과: 예금이 성공되었습니다.");
 	}
 	
 	//출금하기
 	private static void withdraw() {
+		System.out.println("----------");
+		System.out.println("출금");
+		System.out.println("---------");
+		System.out.println("계좌번호: ");
+		
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray == null) {
+				continue;
+			} else if (accountArray[i].getAno().equals(scanner.nextLine())) {
+				System.out.println("출금액: ");
+				accountArray[i].setBalance(accountArray[i].getBalance() - scanner.nextInt());
+				break;
+			}
+		}
+				
+		System.out.println("결과: 출금이 성공되었습니다.");
 		
 	}
 	
 	//Account 배열에서 ano와 동일한 Account 객체 찾기
-	private static Account findAccount(String ano) {
-		
+	private static Account findAccount (String ano) {
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] == null) {
+				continue;
+			} else if (accountArray[i].getAno().equals(ano)) {
+				return accountArray[i];
+			}
+		}
 	}
 }
