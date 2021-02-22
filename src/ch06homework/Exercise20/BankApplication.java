cfedcefa11bef3f5ac0c18d7b5b10a09f047c90d
seoyoung78 <tjdud0708@naver.com>
2021-02-22 21:29:46 +0900

    과제3

--- src/ch06homework/Exercise20/BankApplication.java
+++ src/ch06homework/Exercise20/BankApplication.java
@@ -12,7 +12,7 @@ public class BankApplication {
 			System.out.println("-----------------------------------------");
 			System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
 			System.out.println("-----------------------------------------");
-			System.out.println("선택> ");
+			System.out.print("선택> ");
 			
 			int selectNo = scanner.nextInt();
 			
@@ -33,17 +33,47 @@ public class BankApplication {
 	
 	//계좌생성하기
 	private static void createAccount() {
+		System.out.println("----------");
+		System.out.println("계좌생성");
+		System.out.println("---------");
+		
+		for (int i = 0; i <= accountArray.length; i++) {
+			if (accountArray[i] == null ) {
+				System.out.print("계좌변호: ");
+				accountArray[i].setAno(scanner.nextLine());
+				System.out.print("계좌주: ");
+				accountArray[i].setOwner(scanner.nextLine());
+				System.out.print("초기입금액: ");
+				accountArray[i].setBalance(scanner.nextLine());
+				System.out.println("결과: 계좌가 생성되었습니다.");
+				break;
+			}
+		}
 		
 	}
 	
 	//계좌목록보기
 	private static void accountList() {
+		System.out.println("----------");
+		System.out.println("계좌목록");
+		System.out.println("---------");
 		
+		for (int i = 0; i <= accountArray.length; i++) {
+			System.out.println(accountArray[i].getAno() + "\t"
+								+ accountArray[i].getOwner() + "\t"
+								+ accountArray[i].getBalance());
+		}
 	}
 	
 	//예금하기
 	private static void deposit() {
+		System.out.println("----------");
+		System.out.println("예금");
+		System.out.println("---------");
+		System.out.println("계좌번호: ");
 		
+		
+		System.out.println("결과: 예금이 성공되었습니다.");
 	}
 	
 	//출금하기
