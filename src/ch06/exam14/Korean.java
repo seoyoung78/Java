cd394351d912df9695ae88069f4582553960930a
seoyoung78 <tjdud0708@naver.com>
2021-02-22 17:35:01 +0900

    2/22 오후 강의 추가

---
+++ src/ch06/exam14/Korean.java
@@ -0,0 +1,15 @@
+package ch06.exam14;
+
+public class Korean {
+	//Field
+	final static String NATION = "ÇÑ±¹";
+	final String ssn;
+	
+	//Constructor
+	public Korean(String ssn) {
+		this.ssn = ssn;
+	}
+	
+	//Method
+	
+}
---
+++ src/ch06homework/Student.java
@@ -0,0 +1,5 @@
+package ch06homework;
+
+public class Student {
+
+}
