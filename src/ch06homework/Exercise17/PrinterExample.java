1395a4123f3f73c8dd9f5c0072c623eb69dbb5ec
seoyoung78 <tjdud0708@naver.com>
2021-02-22 21:12:16 +0900

    과제2

--- src/ch06homework/Exercise17/PrinterExample.java
+++ src/ch06homework/Exercise17/PrinterExample.java
@@ -3,11 +3,10 @@ package ch06homework.Exercise17;
 public class PrinterExample {
 
 	public static void main(String[] args) {
-		
-		printer.println(10);
-		printer.println(true);
-		printer.println(5.7);
-		printer.println("ȫ�浿");
+		Printer.println(10);
+		Printer.println(true);
+		Printer.println(5.7);
+		Printer.println("ȫ�浿");
 	}
 
 }
